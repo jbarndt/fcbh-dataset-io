0095f8cec30b54e0c0e0cd4cd1d7d3f6e37a8918
gary <gary@shortsands.com>
2024-02-28 01:33:46 -0500

    Wrote WhisperAdapter

---
+++ WhisperAdapter.py
@@ -0,0 +1,73 @@
+import os
+import sys
+import whisper
+from DBAdapter import *
+from FileAdapter import *
+from Booknames import *
+
+#https://github.com/openai/whisper
+
+#model = whisper.load_model("base")
+#result = model.transcribe("audio.mp3")
+#print(result["text"])
+
+class WhisperAdapter:
+
+
+	def __init__(self, db):
+		self.db = db
+		self.model = whisper.load_model("base") # "small" "medium" "large" are options
+		self.books = Booknames()
+
+
+	def insertIdent(self, bibleId):
+		iso = bibleId[:3]
+		version = bibleId[3:]
+		self.db.insertIdent(bibleId, iso, version, "WHISPER", None, None, None, None, None)
+
+
+	def processDirectory(self, directory):
+		for file in sorted(os.listdir(directory)):
+			if not file.startswith("."):
+				print(file)
+				resultSet = self.db.selectScriptsByFile(file)
+				print("result")
+				if len(resultSet) == 0:
+					if file.startswith("B17"):
+						self.processFile(directory, file)
+
+
+	def processFile(self, directory, filename):
+		filePath = os.path.join(directory, filename)
+		(bookId, chapter) = self.parseFilename(filename)
+		result = self.model.transcribe(filePath)
+		scriptText = result["text"]
+		self.db.addScript(bookId, chapter, filename, 1, None, None, None, None, scriptText)
+		self.db.insertScripts()
+
+
+	def parseFilename(self, filename):
+		chapter = int(filename[6:8])
+		bookName = filename[9:21].replace("_", "")
+		bookId = self.books.usfmBookId(bookName)
+		return (bookId, chapter)
+
+
+if __name__ == "__main__":
+	if len(sys.argv) < 2:
+		print("Usage: WhisperAdapter  bibleId")
+		sys.exit(1)
+	bibleId = sys.argv[1]
+	db = DBAdapter(bibleId + "_WHISPER.db")
+	whisp = WhisperAdapter(db)
+	filesetId = bibleId + "N2DA"
+	directory = os.path.join(os.environ['FCBH_DATASET_FILES'], bibleId, filesetId)
+	whisp.processDirectory(directory)
+	file = FileAdapter(db)
+	file.loadWords()
+	db.close()
+
+
+
+
+
