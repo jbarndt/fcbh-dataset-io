1cdad5451b034030a0725b4e6a6ab052dfd94efd
gary <gary@shortsands.com>
2024-04-10 23:18:53 -0400

    Nearly finished with Validation

--- req/validate.go
+++ req/validate.go
@@ -10,11 +10,21 @@ func Validate(req Request) {
 	checkRequired(req.Required, &msgs)
 	checkTestament(&req.Testament)
 	checkAudioData(&req.AudioData, &msgs)
-	//checkTextData(&req.TextData, &msgs)
+	checkTextData(&req.TextData, &msgs)
+	checkDetail(&req.Detail)
+	checkTimestamps(&req.Timestamps, &msgs)
+	checkAudioEncoding(&req.AudioEncoding, &msgs)
+	checkTextEncoding(&req.TextEncoding, &msgs)
+	checkOutputFormat(&req.OutputFormat, &msgs)
+	checkCompare(req.Compare, &msgs)
+	checkCompareSettings(req.Compare.CompareSettings, &msgs)
+	fmt.Println("")
 	for _, msg := range msgs {
 		fmt.Println(msg)
 	}
-	fmt.Println(req.Testament)
+	fmt.Println("Testament", req.Testament)
+	fmt.Println("Detail", req.Detail)
+	fmt.Println("Timestamps", req.Timestamps)
 }
 
 func checkRequired(req Required, msgs *[]string) {
@@ -56,32 +66,64 @@ func checkTextData(req *TextData, msgs *[]string) {
 	}
 }
 
-func checkForOne(sVal reflect.Value, msgs *[]string) int {
+func checkDetail(req *Detail) {
+	if !req.Lines && !req.Words {
+		req.Lines = true
+	}
+}
+
+func checkTimestamps(req *Timestamps, msgs *[]string) {
+	structVal := reflect.ValueOf(*req)
+	count := checkForOne(structVal, msgs)
+	if count == 0 {
+		req.NoTimestamps = true
+	}
+}
+
+func checkAudioEncoding(req *AudioEncoding, msgs *[]string) {
+	structVal := reflect.ValueOf(*req)
+	count := checkForOne(structVal, msgs)
+	if count == 0 {
+		req.NoEncoding = true
+	}
+}
+
+func checkTextEncoding(req *TextEncoding, msgs *[]string) {
+	structVal := reflect.ValueOf(*req)
+	count := checkForOne(structVal, msgs)
+	if count == 0 {
+		req.NoEncoding = true
+	}
+}
+
+func checkOutputFormat(req *OutputFormat, msgs *[]string) {
+	structVal := reflect.ValueOf(*req)
+	count := checkForOne(structVal, msgs)
+	if count == 0 {
+		req.JSON = true
+	}
+}
+
+func checkCompare(req Compare, msgs *[]string) {
+	if (req.Project1 != `` && req.Project2 == ``) ||
+		(req.Project2 != `` && req.Project1 == ``) {
+		*msgs = append(*msgs, `Compare must have two projects, not one.`)
+	}
+}
+
+func checkCompareSettings(req CompareSettings, msgs *[]string) {
+	checkForOne(reflect.ValueOf(req.DoubleQuotes), msgs)
+	structVal := reflect.ValueOf(req.Apostrophe)
+	checkForOne(structVal, msgs)
+	checkForOne(reflect.ValueOf(req.Hyphen), msgs)
+	checkForOne(reflect.ValueOf(req.DiacriticalMarks), msgs)
+}
+
+func checkForOne(structVal reflect.Value, msgs *[]string) int {
 	var errorCount int
 	var wasSet []string
-	for i := 0; i < sVal.NumField(); i++ {
-		field := sVal.Field(i)
-		if field.Kind() == reflect.String {
-			if field.String() != `` {
-				wasSet = append(wasSet, sVal.Type().Name()+`.`+sVal.Type().Field(i).Name)
-			}
-		} else if field.Kind() == reflect.Bool {
-			if field.Bool() {
-				wasSet = append(wasSet, sVal.Type().Name()+`.`+sVal.Type().Field(i).Name)
-			}
-		} else if field.Kind() == reflect.Struct {
-			for j := 0; j < field.NumField(); j++ {
-				bbField := field.Field(j)
-				if bbField.Bool() {
-					wasSet = append(wasSet, field.Type().Name()+`.`+field.Type().Field(j).Name)
-				}
-			}
-		} else {
-			msg := sVal.Type().Field(i).Name + ` has unexpected type ` + field.Type().Name()
-			panic(msg)
-		}
-	}
-	if len(wasSet) > 0 {
+	checkForOneRecursive(structVal, &wasSet)
+	if len(wasSet) > 1 {
 		for _, item := range wasSet {
 			errorCount++
 			msg := `Only 1 Data field can be set ` + item
@@ -89,4 +131,24 @@ func checkForOne(sVal reflect.Value, msgs *[]string) int {
 		}
 	}
 	return errorCount
+}
+
+func checkForOneRecursive(sVal reflect.Value, wasSet *[]string) {
+	for i := 0; i < sVal.NumField(); i++ {
+		field := sVal.Field(i)
+		if field.Kind() == reflect.String {
+			if field.String() != `` {
+				*wasSet = append(*wasSet, sVal.Type().Name()+`.`+sVal.Type().Field(i).Name)
+			}
+		} else if field.Kind() == reflect.Bool {
+			if field.Bool() {
+				*wasSet = append(*wasSet, sVal.Type().Name()+`.`+sVal.Type().Field(i).Name)
+			}
+		} else if field.Kind() == reflect.Struct {
+			checkForOneRecursive(field, wasSet)
+		} else {
+			msg := sVal.Type().Field(i).Name + ` has unexpected type ` + field.Type().Name()
+			panic(msg)
+		}
+	}
 }
--- req/validate_test.go
+++ req/validate_test.go
@@ -10,6 +10,18 @@ func TestValidate(t *testing.T) {
 	req.Required.VersionCode = `WBT`
 	req.AudioData.File = `file:///where`
 	req.AudioData.BibleBrain.MP3_64 = true
+	req.AudioData.BibleBrain.OPUS = true
 	req.AudioData.POST = true
+	req.TextData.NoText = true
+	req.TextData.BibleBrain.TextPlain = true
+	req.TextData.SpeechToText.Whisper.Model.Medium = true
+	req.AudioEncoding.MFCC = true
+	req.AudioEncoding.NoEncoding = true
+	req.Compare.CompareSettings.Apostrophe.Normalize = true
+	req.Compare.CompareSettings.Apostrophe.Remove = true
 	Validate(req)
 }
+
+// I should have a test with multiple error
+// I shoud have a test with one selected, not error
+// I should have a test with none selected
