5f1bddbfd544b5d99327e8dc09bf0b875b63be30
gary <gary@shortsands.com>
2024-04-15 13:34:14 -0400

    Changed interface back to readFiles only

--- read/dbp_text_reader.go
+++ read/dbp_text_reader.go
@@ -38,72 +38,63 @@ func (d *DBPTextReader) ProcessFiles(files []InputFiles) dataset.Status {
 		if err != nil {
 			return log.Error(d.ctx, 500, err, "Error reading file:", filePath)
 		}
-		status = d.Process(content, ``, 0, ``)
-		if status.IsErr {
-			return status
+		var scriptNum = 0
+		var lastBookId string
+		type TempRec struct {
+			BookId     string `json:"book_id"`
+			BookSeq    int
+			ChapterNum int    `json:"chapter"`
+			VerseStart int    `json:"verse_start"`
+			VerseEnd   int    `json:"verse_end"`
+			Text       string `json:"verse_text"`
 		}
-	}
-	return status
-}
-
-func (d DBPTextReader) Process(content []byte, bookId string, chapter int, verse string) dataset.Status {
-	var status dataset.Status
-	var scriptNum = 0
-	var lastBookId string
-	type TempRec struct {
-		BookId     string `json:"book_id"`
-		BookSeq    int
-		ChapterNum int    `json:"chapter"`
-		VerseStart int    `json:"verse_start"`
-		VerseEnd   int    `json:"verse_end"`
-		Text       string `json:"verse_text"`
-	}
-	type TempResp struct {
-		Data []TempRec `json:"data"`
-	}
-	var response TempResp
-	err := json.Unmarshal(content, &response)
-	if err != nil {
-		return log.Error(d.ctx, 500, err, "Error parsing JSON from plain_text")
-	}
-	var verses = response.Data
-	for i, vs := range verses {
-		verses[i].BookSeq = db.BookSeqMap[vs.BookId]
-	}
-	sort.Slice(verses, func(i, j int) bool {
-		if verses[i].BookSeq != verses[j].BookSeq {
-			return verses[i].BookSeq < verses[j].BookSeq
+		type TempResp struct {
+			Data []TempRec `json:"data"`
 		}
-		if verses[i].ChapterNum != verses[j].ChapterNum {
-			return verses[i].ChapterNum < verses[j].ChapterNum
+		var response TempResp
+		err = json.Unmarshal(content, &response)
+		if err != nil {
+			return log.Error(d.ctx, 500, err, "Error parsing JSON from plain_text")
 		}
-		return verses[i].VerseStart < verses[j].VerseStart
-	})
-	var records = make([]db.Script, 0, 1000)
-	for _, vs := range verses {
-		if d.testament.HasNT(vs.BookId) || d.testament.HasOT(vs.BookId) {
-			scriptNum++
-			if vs.BookId != lastBookId {
-				//fmt.Println(vs.BookId)
-				lastBookId = vs.BookId
-				scriptNum = 1
+		var verses = response.Data
+		for i, vs := range verses {
+			verses[i].BookSeq = db.BookSeqMap[vs.BookId]
+		}
+		sort.Slice(verses, func(i, j int) bool {
+			if verses[i].BookSeq != verses[j].BookSeq {
+				return verses[i].BookSeq < verses[j].BookSeq
 			}
-			var rec db.Script
-			rec.ScriptNum = strconv.Itoa(scriptNum)
-			rec.BookId = vs.BookId
-			rec.ChapterNum = vs.ChapterNum
-			rec.VerseNum = vs.VerseStart
-			if vs.VerseStart == vs.VerseEnd {
-				rec.VerseStr = strconv.Itoa(vs.VerseStart)
-			} else {
-				rec.VerseStr = strconv.Itoa(vs.VerseStart) + `-` + strconv.Itoa(vs.VerseEnd)
+			if verses[i].ChapterNum != verses[j].ChapterNum {
+				return verses[i].ChapterNum < verses[j].ChapterNum
 			}
-			text := strings.Replace(vs.Text, "&lt", "<", -1)
-			text = strings.Replace(text, "&gt", ">", -1)
-			rec.ScriptTexts = append(rec.ScriptTexts, text)
-			records = append(records, rec)
+			return verses[i].VerseStart < verses[j].VerseStart
+		})
+		var records = make([]db.Script, 0, 1000)
+		for _, vs := range verses {
+			if d.testament.HasNT(vs.BookId) || d.testament.HasOT(vs.BookId) {
+				scriptNum++
+				if vs.BookId != lastBookId {
+					//fmt.Println(vs.BookId)
+					lastBookId = vs.BookId
+					scriptNum = 1
+				}
+				var rec db.Script
+				rec.ScriptNum = strconv.Itoa(scriptNum)
+				rec.BookId = vs.BookId
+				rec.ChapterNum = vs.ChapterNum
+				rec.VerseNum = vs.VerseStart
+				if vs.VerseStart == vs.VerseEnd {
+					rec.VerseStr = strconv.Itoa(vs.VerseStart)
+				} else {
+					rec.VerseStr = strconv.Itoa(vs.VerseStart) + `-` + strconv.Itoa(vs.VerseEnd)
+				}
+				text := strings.Replace(vs.Text, "&lt", "<", -1)
+				text = strings.Replace(text, "&gt", ">", -1)
+				rec.ScriptTexts = append(rec.ScriptTexts, text)
+				records = append(records, rec)
+			}
 		}
+		status = d.conn.InsertScripts(records)
 	}
-	status = d.conn.InsertScripts(records)
 	return status
 }
