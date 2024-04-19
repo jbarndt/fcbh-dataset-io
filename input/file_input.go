b6b8528f9fcdad37611b044a2156a4913ae88c6a
gary <gary@shortsands.com>
2024-04-19 15:21:09 -0400

    file_input is unit tested.

--- input/file_input.go
+++ input/file_input.go
@@ -3,20 +3,26 @@ package input
 import (
 	"context"
 	"dataset"
+	"dataset/request"
 )
 
-func FileInput(ctx context.Context, path string) ([]InputFile, dataset.Status) {
+func FileInput(ctx context.Context, path string, testament request.Testament) ([]InputFile, dataset.Status) {
 	var files []InputFile
 	var status dataset.Status
 	files, status = Glob(ctx, path)
 	if status.IsErr {
 		return files, status
 	}
-	for _, file := range files {
-		status = SetMediaType(ctx, &file)
+	for i, _ := range files {
+		status = SetMediaType(ctx, &files[i])
+		if status.IsErr {
+			return files, status
+		}
+		status = ParseFilenames(ctx, &files[i])
 		if status.IsErr {
 			return files, status
 		}
 	}
-	return files, status
+	inputFiles := PruneBooksByRequest(files, testament)
+	return inputFiles, status
 }
---
+++ input/file_input_test.go
@@ -0,0 +1,57 @@
+package input
+
+import (
+	"context"
+	"dataset/request"
+	"os"
+	"path/filepath"
+	"testing"
+)
+
+func TestPlainTextFileInput(t *testing.T) {
+	ctx := context.Background()
+	bibleId := `ENGWEB`
+	testament := request.Testament{OTBooks: []string{`MAL`, `JON`}, NTBooks: []string{`TIT`, `REV`}}
+	testament.BuildBookMaps()
+	search := filepath.Join(os.Getenv(`FCBH_DATASET_FILES`), bibleId, `*_ET.json`)
+	files, status := FileInput(ctx, search, testament)
+	if status.IsErr {
+		t.Error(status.Message)
+	}
+	if len(files) != 2 {
+		t.Error(`Test should have found 2 files`, len(files))
+	}
+}
+
+func TestUSXTextFileInput(t *testing.T) {
+	ctx := context.Background()
+	bibleId := `ENGWEB`
+	filesetId := `ENGWEBO_ET-usx`
+	testament := request.Testament{OTBooks: []string{`MAL`, `JON`, `GEN`}, NTBooks: []string{`TIT`, `REV`}}
+	testament.BuildBookMaps()
+	search := filepath.Join(os.Getenv(`FCBH_DATASET_FILES`), bibleId, filesetId, `*.usx`)
+	files, status := FileInput(ctx, search, testament)
+	if status.IsErr {
+		t.Error(status.Message)
+	}
+	if len(files) != 3 {
+		t.Error(`Test should have found 3 files`, len(files))
+	}
+}
+
+func TestAudioFileInput(t *testing.T) {
+	ctx := context.Background()
+	bibleId := `ENGWEB`
+	filesetId := `ENGWEBN2DA-mp3-64`
+	testament := request.Testament{OTBooks: []string{`MAL`, `JON`, `GEN`}, NTBooks: []string{`TIT`, `REV`}}
+	testament.BuildBookMaps()
+	search := filepath.Join(os.Getenv(`FCBH_DATASET_FILES`), bibleId, filesetId, `*.mp3`)
+	files, status := FileInput(ctx, search, testament)
+	if status.IsErr {
+		t.Error(status.Message)
+	}
+	if len(files) != 25 {
+		t.Error(`Test should have found 25 files`, len(files))
+	}
+	//fmt.Println(files)
+}
