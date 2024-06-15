e43f8708511a23215c3df30aa6f04593ba398eb1
gary <gary@shortsands.com>
2024-06-15 17:33:16 -0400

    Compare Test

--- match/compare_test.go
+++ match/compare_test.go
@@ -10,23 +10,36 @@ import (
 )
 
 func TestCompare(t *testing.T) {
-	ctx := context.Background()
-	user, _ := fetch.GetDBPUser()
-	user.Username = ``
-	var testament = request.Testament{NT: true}
-	var cfg request.CompareSettings
-	cfg.LowerCase = true
-	cfg.RemovePromptChars = true
-	cfg.RemovePunctuation = true
-	cfg.DoubleQuotes.Remove = true
-	cfg.Apostrophe.Remove = true
-	cfg.Hyphen.Remove = true
-	cfg.DiacriticalMarks.NormalizeNFD = true
-	conn := db.NewDBAdapter(ctx, `ATIWBT_SCRIPT.db`)
-	compare := NewCompare(ctx, user, `ATIWBT_USXEDIT`, conn, testament, cfg)
-	filename, status := compare.Process()
-	fmt.Println(status, filename)
-	if compare.diffCount != 2 {
-		t.Error(`Expected count is 2, actual was`, compare.diffCount)
+	type testCase struct {
+		baseDB  string
+		project string
+		expect  int
+	}
+	var tests []testCase
+	//tests = append(tests, testCase{baseDB: "ATIWBT_SCRIPT.db", project: "ATIWBT_USXEDIT", expect: 2})
+	//tests = append(tests, testCase{baseDB: `ENGWEB_WHISPER.db`, project: `ENGWEB_WHISPER_STT`, expect: 90})
+	//tests = append(tests, testCase{baseDB: `APFCMU_WHISPER.db`, project: `APFCMU_WHISPER_STT`, expect: 91})
+	tests = append(tests, testCase{baseDB: `DYIIBS_WHISPER.db`, project: `DYIIBS_WHISPER_STT`, expect: 91})
+
+	for _, tst := range tests {
+		ctx := context.Background()
+		user, _ := fetch.GetTestUser()
+		user.Username = ``
+		var testament = request.Testament{NT: true}
+		var cfg request.CompareSettings
+		cfg.LowerCase = true
+		cfg.RemovePromptChars = true
+		cfg.RemovePunctuation = true
+		cfg.DoubleQuotes.Remove = true
+		cfg.Apostrophe.Remove = true
+		cfg.Hyphen.Remove = true
+		cfg.DiacriticalMarks.NormalizeNFD = true
+		conn := db.NewDBAdapter(ctx, tst.baseDB)
+		compare := NewCompare(ctx, user, tst.project, conn, testament, cfg)
+		filename, status := compare.Process()
+		fmt.Println(status, filename)
+		if compare.diffCount != tst.expect {
+			t.Error(`Expected count is`, tst.expect, ` actual was`, compare.diffCount)
+		}
 	}
 }
