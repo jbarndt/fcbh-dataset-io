9d0ece2662d124f8ac924f52a4db8d01dd09ec9c
gary <gary@shortsands.com>
2024-10-15 13:16:08 -0400

    wrote mms_asr.py

--- mms/mms_asr_hug.py
+++
@@ -1,9 +0,0 @@
-# https://huggingface.co/facebook/mms-1b-all
-
-# conda install datasets
-from datasets import load_dataset, Audio
-
-# English
-stream_data = load_dataset("mozilla-foundation/common_voice_13_0", "en", split="test", streaming=True, trust_remote_code=True)
-stream_data = stream_data.cast_column("audio", Audio(sampling_rate=16000))
-en_sample = next(iter(stream_data))["audio"]["array"]
--- mms/mms_stt.py
+++
@@ -1,4 +0,0 @@
-
-
-# Documentation used to write this program
-# https://huggingface.co/docs/transformers/main/en/model_doc/mms
