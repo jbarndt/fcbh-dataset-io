65c79694cb3c458316897ad688c8cfb272a68137
gary <gary@shortsands.com>
2024-10-14 20:35:03 -0400

    wrote mms_lid

--- mms/mms_lid.py
+++ mms/mms_lid.py
@@ -6,26 +6,32 @@ import torch
 # This code comes from the following page on huggingface
 # https://huggingface.co/facebook/mms-lid-4017
 
-# Function to identify the language of an audio file
-def identify_language(audioFile: str) -> str:
-    fromDict = Dataset.from_dict({"audio": [audioFile]})
-    streamData = fromDict.cast_column("audio", Audio())
-    sample = next(iter(streamData))["audio"]["array"]
+class MMSLanguageIdentifier:
 
-    model_id = "facebook/mms-lid-4017"
-    processor = AutoFeatureExtractor.from_pretrained(model_id)
-    model = Wav2Vec2ForSequenceClassification.from_pretrained(model_id)
-    inputs = processor(sample, sampling_rate=16_000, return_tensors="pt")
-    with torch.no_grad():
-        outputs = model(**inputs).logits
-    lang_id = torch.argmax(outputs, dim=-1)[0].item()
-    detected_lang = model.config.id2label[lang_id]
-    return detected_lang
+    ## create an instance of a language identifier to be used any number of times
+    def __init__(self):
+        model_id = "facebook/mms-lid-4017"
+        self.processor = AutoFeatureExtractor.from_pretrained(model_id)
+        self.model = Wav2Vec2ForSequenceClassification.from_pretrained(model_id)
+
+
+    # Function to identify the language of an audio file
+    def identify(self, audioFile: str) -> str:
+        fromDict = Dataset.from_dict({"audio": [audioFile]})
+        streamData = fromDict.cast_column("audio", Audio())
+        sample = next(iter(streamData))["audio"]["array"]
+        inputs = self.processor(sample, sampling_rate=16_000, return_tensors="pt")
+        with torch.no_grad():
+            outputs = self.model(**inputs).logits
+        lang_id = torch.argmax(outputs, dim=-1)[0].item()
+        detected_lang = self.model.config.id2label[lang_id]
+        return detected_lang
 
 
 if __name__ == "__main__":
     audioPath = os.environ.get("FCBH_DATASET_FILES") + "/NPIDPI/NPIDPIN1DA/B02___01_Mark________NPIDPIN1DA.wav"
-    language = identify_language(audioPath)
+    ident = MMSLanguageIdentifier()
+    language = ident.identify(audioPath)
     print("language", language)
 
 
---
+++ mms/mms_stt.py
@@ -0,0 +1,4 @@
+
+
+# Documentation used to write this program
+# https://huggingface.co/docs/transformers/main/en/model_doc/mms
