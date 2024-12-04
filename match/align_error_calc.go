19e6aff4e597eba2effb1edf3c8ea4da1e07a238
gary <gary@shortsands.com>
2024-12-03 22:23:39 -0500

    align_error_calc rewritten

--- match/align_error_calc.go
+++ match/align_error_calc.go
@@ -4,27 +4,37 @@ import (
 	"context"
 	"dataset"
 	"dataset/db"
-	"sort"
+	"fmt"
+	"gonum.org/v1/gonum/stat"
+	"math"
 )
 
-type FAverse struct {
-	scriptId   int64
-	bookId     string
-	chapter    int
-	verseStr   string
-	verseSeq   int
-	beginTS    float64
-	endTS      float64
-	faScore    float64
-	words      []db.Audio
-	critWords  []int
-	critScore  float64
-	questWords []int
-	questScore float64
-	endTSDiff  float64
-	critDiff   bool
-	questDiff  bool
-}
+const (
+	criticalThreshold = 0.001
+	questionThreshold = 0.01
+)
+
+type ErrorType int
+
+const (
+	noError ErrorType = iota
+	scoreCritical
+	scoreQuestion
+	durationLong
+	betweenCharsLong
+	betweenWordsLong
+	betweenVersesLong
+	betweenChaptersLong
+)
+
+type SilencePosition int
+
+const (
+	betweenChars SilencePosition = iota + 1
+	betweenWords
+	betweenVerses
+	betweenChapters
+)
 
 type AlignErrorCalc struct {
 	ctx  context.Context
@@ -38,116 +48,136 @@ func NewAlignErrorCalc(ctx context.Context, conn db.DBAdapter) AlignErrorCalc {
 	return a
 }
 
-func (a *AlignErrorCalc) Process() ([]FAverse, dataset.Status) {
-	var verses []FAverse
+func (a *AlignErrorCalc) Process() ([]db.AlignChar, dataset.Status) {
+	var faChars []db.AlignChar
 	var status dataset.Status
-	var words []db.Audio
-	words, status = a.conn.SelectFAWordTimestamps()
+	faChars, status = a.conn.SelectFACharTimestamps()
 	if status.IsErr {
-		return verses, status
+		return faChars, status
 	}
-	verses = a.createVerseSlice(words)
-	verseMap := a.createVerseMap(verses)
-	verses = a.addWordsToVerses(verseMap, words)
-	a.computeCritical(verses, 0.0001)
-	a.computeQuestion(verses, 0.001)
-	a.computeTSDiff(verses)
-	a.markCriticalDiff(verses, 1.5)
-	a.markQuestionDiff(verses, 0.9)
-	return verses, status
-}
-
-func (a *AlignErrorCalc) createVerseSlice(words []db.Audio) []FAverse {
-	var result []FAverse
-	var priorScriptId int64 = -1
-	for _, w := range words {
-		if w.ScriptId != priorScriptId {
-			priorScriptId = w.ScriptId
-			var v FAverse
-			v.scriptId = w.ScriptId
-			v.bookId = w.BookId
-			v.chapter = w.ChapterNum
-			v.verseStr = w.VerseStr
-			v.verseSeq = w.VerseSeq
-			v.beginTS = w.ScriptBeginTS
-			v.endTS = w.ScriptEndTS
-			v.faScore = w.ScriptFAScore
-			result = append(result, v)
+	for i := range faChars {
+		if faChars[i].FAScore <= criticalThreshold {
+			faChars[i].ScoreError = int(scoreCritical)
+		} else if faChars[i].FAScore <= questionThreshold {
+			faChars[i].ScoreError = int(scoreQuestion)
 		}
 	}
-	return result
-}
-
-func (a *AlignErrorCalc) createVerseMap(verses []FAverse) map[int64]FAverse {
-	var result = make(map[int64]FAverse)
-	for _, vs := range verses {
-		result[vs.scriptId] = vs
+	for i := range faChars {
+		faChars[i].Duration = faChars[i].EndTS - faChars[i].BeginTS
 	}
-	return result
-}
-
-func (a *AlignErrorCalc) addWordsToVerses(verseMap map[int64]FAverse, words []db.Audio) []FAverse {
-	for _, w := range words {
-		vs, ok := verseMap[w.ScriptId]
-		if !ok {
-			panic("what??")
-		}
-		vs.words = append(vs.words, w)
-		verseMap[w.ScriptId] = vs
-	}
-	var result []FAverse
-	for _, vs := range verseMap {
-		result = append(result, vs)
-	}
-	sort.Slice(result, func(i, j int) bool {
-		return result[i].scriptId < result[j].scriptId
-	})
-	return result
-}
-
-func (a *AlignErrorCalc) computeCritical(verses []FAverse, threshold float64) {
-	for i := range verses {
-		verses[i].critWords, verses[i].critScore = a.findErrors(verses[i].words, threshold)
-	}
-}
-
-func (a *AlignErrorCalc) computeQuestion(verses []FAverse, threshold float64) {
-	for i := range verses {
-		verses[i].questWords, verses[i].questScore = a.findErrors(verses[i].words, threshold)
-	}
-}
-
-func (a *AlignErrorCalc) findErrors(words []db.Audio, threshold float64) ([]int, float64) {
-	var results []int
-	var score float64
-	for i, w := range words {
-		if w.FAScore <= threshold {
-			results = append(results, i)
-			score += (1.0 - w.FAScore) * float64(len(w.Text))
+	for i := 0; i < len(faChars)-1; i++ {
+		var curr = faChars[i]
+		var next = faChars[i+1]
+		faChars[i].Silence = next.BeginTS - curr.EndTS
+		if curr.WordId == next.WordId {
+			faChars[i].SilencePos = int(betweenChars)
+		} else if curr.ScriptId == next.ScriptId {
+			faChars[i].SilencePos = int(betweenWords)
+		} else if curr.BookId == next.BookId && curr.ChapterNum == next.ChapterNum {
+			faChars[i].SilencePos = int(betweenVerses)
+		} else {
+			faChars[i].SilencePos = int(betweenChapters)
+			faChars[i].Silence = next.BeginTS
+			// To be correct, I should add the silence of fileDuration - curr.EndTS
 		}
 	}
-	return results, score
+	mean, stddev, mini, maxi := a.analyzeData(a.getDurations(faChars))
+	fmt.Println("Duration:", mean, stddev, mini, maxi)
+	a.markDurationOutliers(faChars, mean, stddev)
+	mean, stddev, mini, maxi = a.analyzeData(a.getSilence(faChars, betweenChars))
+	fmt.Println("Between Chars:", mean, stddev, mini, maxi)
+	a.markSilenceOutliers(faChars, mean, stddev, betweenChars, betweenCharsLong)
+	mean, stddev, mini, maxi = a.analyzeData(a.getSilence(faChars, betweenWords))
+	fmt.Println("Between Words:", mean, stddev, mini, maxi)
+	a.markSilenceOutliers(faChars, mean, stddev, betweenWords, betweenWordsLong)
+	mean, stddev, mini, maxi = a.analyzeData(a.getSilence(faChars, betweenVerses))
+	fmt.Println("Between Verses:", mean, stddev, mini, maxi)
+	a.markSilenceOutliers(faChars, mean, stddev, betweenVerses, betweenVersesLong)
+	mean, stddev, mini, maxi = a.analyzeData(a.getSilence(faChars, betweenChapters))
+	fmt.Println("Between Chapters:", mean, stddev, mini, maxi)
+	a.markSilenceOutliers(faChars, mean, stddev, betweenChapters, betweenChaptersLong)
+	return faChars, status
 }
 
-func (a *AlignErrorCalc) computeTSDiff(verses []FAverse) {
-	for i := range verses {
-		lastWord := verses[i].words[len(verses[i].words)-1]
-		verses[i].endTSDiff = verses[i].endTS - lastWord.EndTS
+func (a *AlignErrorCalc) getDurations(chars []db.AlignChar) []float64 {
+	var data []float64
+	for _, ch := range chars {
+		data = append(data, float64(ch.Duration))
 	}
+	return data
 }
 
-func (a *AlignErrorCalc) markCriticalDiff(verses []FAverse, threshold float64) {
-	for i := range verses {
-		if verses[i].endTSDiff >= threshold {
-			verses[i].critDiff = true
+func (a *AlignErrorCalc) getSilence(chars []db.AlignChar, pos SilencePosition) []float64 {
+	var data []float64
+	posInt := int(pos)
+	for _, ch := range chars {
+		if ch.SilencePos == posInt {
+			data = append(data, float64(ch.Silence))
+		}
+	}
+	return data
+}
+
+func (a *AlignErrorCalc) analyzeData(data []float64) (mean, stddev, min, max float64) {
+	mean = stat.Mean(data, nil)
+	stddev = stat.StdDev(data, nil)
+	min = data[0]
+	max = data[0]
+	for _, v := range data[1:] {
+		min = math.Min(min, v)
+		max = math.Max(max, v)
+	}
+	return mean, stddev, min, max
+}
+
+func (a *AlignErrorCalc) markDurationOutliers(chars []db.AlignChar, mean float64, stddev float64) {
+	var pct95 = mean + 2.2*stddev
+	for i := range chars {
+		if chars[i].Duration > pct95 {
+			chars[i].DurationLong = int(durationLong)
 		}
 	}
 }
 
-func (a *AlignErrorCalc) markQuestionDiff(verses []FAverse, threshold float64) {
-	for i := range verses {
-		if verses[i].endTSDiff >= threshold {
-			verses[i].questDiff = true
+func (a *AlignErrorCalc) markSilenceOutliers(chars []db.AlignChar, mean float64, stddev float64,
+	silencePos SilencePosition, errorType ErrorType) {
+	var pct95 = mean + 2.0*stddev
+	for i := range chars {
+		if chars[i].SilencePos == int(silencePos) {
+			if chars[i].Silence > pct95 {
+				if chars[i].SilenceLong == 0 {
+					chars[i].SilenceLong = int(errorType)
+				} else {
+					fmt.Println("Skip Long Silence", silencePos, chars[i])
+				}
+			}
 		}
 	}
+}
+
+func (a *AlignErrorCalc) countErrors(chars []db.AlignChar) {
+	var critScoreError int
+	var questScoreError int
+	var durationLongCount int
+	var count = make([]int, 8)
+	for _, ch := range chars {
+		if ch.ScoreError == int(scoreCritical) {
+			critScoreError++
+		} else if ch.ScoreError == int(scoreQuestion) {
+			questScoreError++
+		}
+		if ch.DurationLong > 0 {
+			durationLongCount++
+		}
+		count[ch.SilenceLong]++
+	}
+	fmt.Println("NO Error\t", count[noError]-critScoreError-questScoreError-durationLongCount)
+	fmt.Println("ScoreCritical", critScoreError)
+	fmt.Println("ScoreQuestion", questScoreError)
+	fmt.Println("DurationLong", durationLongCount)
+	fmt.Println("BetweenCharsLong", count[betweenCharsLong])
+	fmt.Println("BetweenWordsLong", count[betweenWordsLong])
+	fmt.Println("BetweenVersesLong", count[betweenVersesLong])
+	fmt.Println("BetweenChaptersLong", count[betweenChaptersLong])
+	fmt.Println("Total\t", len(chars))
 }
--- match/align_error_calc_test.go
+++ match/align_error_calc_test.go
@@ -11,15 +11,51 @@ import (
 
 func TestNewAlignErrorCalc(t *testing.T) {
 	ctx := context.Background()
-	dbPath := filepath.Join(os.Getenv("GOPROJ"), "dataset", "match", "N2YPM_JMD.db")
+	//var database = "N2YPM_JMD.db"
+	var database = "ENGWEB.db"
+	dbPath := filepath.Join(os.Getenv("GOPROJ"), "dataset", "match", database)
 	conn := db.NewDBAdapter(ctx, dbPath)
 	calc := NewAlignErrorCalc(ctx, conn)
-	verses, status := calc.Process()
+	faChars, status := calc.Process()
 	if status.IsErr {
 		t.Fatal(status)
 	}
-	for _, vs := range verses {
-		fmt.Printf("%v\n", vs)
-	}
-	fmt.Println(len(verses))
+	calc.countErrors(faChars)
+	fmt.Println(len(faChars))
 }
+
+// TestComputeAvgIntervals is not a Test, but a routine to compute mean and std dev of time between timestamps
+
+/*
+This was useful it updated the database with a silence value
+func TestUpdateCharSilence(t *testing.T) {
+	ctx := context.Background()
+	var database = "ENGWEB.db"
+	dbPath := filepath.Join(os.Getenv("GOPROJ"), "dataset", "match", database)
+	conn := db.NewDBAdapter(ctx, dbPath)
+	defer conn.Close()
+	chars, _ := conn.SelectFACharTimestamps()
+	query := `UPDATE chars SET silence = ? WHERE char_id = ?`
+	tx, err := conn.Begin()
+	if err != nil {
+		t.Fatal(err)
+	}
+	stmt, err := tx.Prepare(query)
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer stmt.Close()
+	for i := 0; i < len(chars)-1; i++ {
+		silence := chars[i+1].charBeginTS - chars[i].charEndTS
+		_, err = stmt.Exec(silence, chars[i].charId)
+		if err != nil {
+			t.Fatal(err)
+		}
+	}
+	err = tx.Commit()
+	if err != nil {
+		t.Fatal(err)
+	}
+}
+
+*/
