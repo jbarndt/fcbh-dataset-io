cec87421f98ec3919fbd475b63c4309f5ee9a28c
gary <gary@shortsands.com>
2024-12-31 15:01:15 -0500

    Correction to align_writer test to be consistent with newAlignWriter

--- match/align_writer_test.go
+++ match/align_writer_test.go
@@ -15,7 +15,7 @@ func TestAlignWriter(t *testing.T) {
 	dbDir := filepath.Join(os.Getenv("GOPROJ"), "dataset", "match")
 	conn := db.NewDBAdapter(ctx, filepath.Join(dbDir, "N2ENGWEB.db"))
 	asrConn := db.NewDBAdapter(ctx, filepath.Join(dbDir, "N2ENGWEB_audio.db"))
-	calc := NewAlignSilence(ctx, conn, asrConn, "eng", "")
+	calc := NewAlignSilence(ctx, conn, asrConn)
 	audioDir := filepath.Join(os.Getenv("FCBH_DATASET_FILES"), "ENGWEB", "ENGWEBN2DA-mp3-64")
 	faLines, filenameMap, status := calc.Process(audioDir)
 	if status.IsErr {
