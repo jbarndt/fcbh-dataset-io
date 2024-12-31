2fb5e6bb8c0eb544c486c2ab5e47296dfb266fbd
gary <gary@shortsands.com>
2024-12-31 15:01:50 -0500

    File copy added to cli_process_entry to be used when output is a database.

--- controller/cli_process_entry.go
+++ controller/cli_process_entry.go
@@ -3,6 +3,7 @@ package controller
 import (
 	"context"
 	"dataset"
+	"io"
 	"os"
 	"strings"
 )
@@ -16,7 +17,12 @@ func CLIProcessEntry(yaml []byte) (string, dataset.Status) {
 		result = status.String()
 	}
 	outputFile := findOutputFilename(yaml)
-	err := os.Rename(filename, outputFile)
+	var err error
+	if strings.HasSuffix(outputFile, ".sqlite") {
+		err = copyFile(filename, outputFile)
+	} else {
+		err = os.Rename(filename, outputFile)
+	}
 	if err != nil {
 		result = filename
 	} else {
@@ -32,4 +38,23 @@ func findOutputFilename(request []byte) string {
 	end := strings.Index(req[start:], "\n")
 	result = strings.TrimSpace(req[start : start+end])
 	return result
+}
+
+func copyFile(src, dst string) error {
+	sourceInfo, err := os.Stat(src)
+	if err != nil {
+		return err
+	}
+	sourceFile, err := os.Open(src)
+	if err != nil {
+		return err
+	}
+	defer sourceFile.Close()
+	destFile, err := os.OpenFile(dst, os.O_RDWR|os.O_CREATE|os.O_TRUNC, sourceInfo.Mode())
+	if err != nil {
+		return err
+	}
+	defer destFile.Close()
+	_, err = io.Copy(destFile, sourceFile)
+	return err
 }
