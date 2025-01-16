35e0d0470002db6451ca1904884a63f05ceb3b2e
Brad Flood <bflood@keyholesoftware.com>
2025-01-16 12:30:04 -0600

    remove secrets;  remove service management

--- docs/user_data.sh
+++ docs/user_data.sh
@@ -182,16 +182,10 @@ echo "# GOPATH" >> $FILE
 echo "export GOPATH=$HOME/go" >> $FILE
 echo "export GOPROJ=$GOPATH/src" >> $FILE
 echo "export PATH=$PATH:/usr/local/go/bin" >> $FILE
-echo "# OPEN_AI" >> $FILE
-echo "export OPENAI_API_KEY=<redacted>" >> $FILE
-echo "# Bible Brain Key" >> $FILE
-echo "export FCBH_DBP_KEY=<redacted>" >> $FILE
 echo "# Data" >> $FILE
 echo "export FCBH_DATASET_DB=$HOME/data" >> $FILE
 echo "export FCBH_DATASET_FILES=$HOME/data/download" >> $FILE
 echo "export FCBH_DATASET_TMP=$HOME/data/tmp" >> $FILE
-echo "export FCBH_DATASET_QUEUE=dataset-queue" >> $FILE
-echo "export FCBH_DATASET_IO_BUCKET=dataset-io" >> $FILE
 echo "# Conda ENVS" >> $FILE
 echo "export FCBH_AENEAS_PYTHON=/opt/conda/envs/aeneas/bin/python" >> $FILE
 echo "export FCBH_FASTTEXT_EXE=/opt/conda/envs/fasttext/fastText/fasttext" >> $FILE
@@ -208,61 +202,6 @@ echo "# <<< end DATASET initialize <<<" >> $FILE
 
 source /home/ec2-user/.bashrc
 
-# ---------- aws credentials ----------
-
-cd /home/ec2-user
-mkdir .aws
-FILE=.aws/credentials
-echo "[default]" > $FILE
-echo "aws_access_key_id = <redacted>" >> $FILE
-echo "aws_secret_access_key = <redacted>" >> $FILE
-
-# ---------- system ctl file ----------
-
-##
-## Update these env values after CLI is tested.
-##
-FILE=$HOME/tmp.service
-echo "[Unit]" > $FILE
-echo 'Description="FCBH Dataset API Server"' >> $FILE
-echo "After=network.target" >> $FILE
-echo "" >> $FILE
-echo "[Service]" >> $FILE
-echo "Type=simple" >> $FILE
-echo "User=dataset" >> $FILE
-echo "# GOPATH" >> $FILE
-echo 'Environment="GOPATH=$HOME/go"' >> $FILE
-echo 'Environment="GOPROJ=$GOPATH/src"' >> $FILE
-echo 'Environment="FCBH_DBP_KEY=<redacted>"' >> $FILE
-echo 'Environment="FCBH_DATASET_DB=$HOME/data"' >> $FILE
-echo 'Environment="FCBH_DATASET_FILES=$HOME/data/download"' >> $FILE
-echo 'Environment="FCBH_DATASET_TMP=$HOME/data/tmp"' >> $FILE
-echo 'Environment="PYTHON_EXE=/opt/conda/envs/aeneas/bin/python"' >> $FILE
-echo 'Environment="FCBH_AENEAS_PYTHON=/opt/conda/envs/aeneas/bin/python"' >> $FILE
-echo 'Environment="FCBH_MMS_PYTHON=/opt/conda/envs/mms_fa/bin/python"' >> $FILE
-echo 'Environment="WHISPER_EXE=/opt/conda/envs/aeneas/bin/whisper"' >> $FILE
-echo 'Environment="FCBH_DATASET_LOG_FILE=/home/dataset/dataset.log"' >> $FILE
-echo 'Environment="FCBH_DATASET_LOG_LEVEL=DEBUG"' >> $FILE
-echo "ExecStart=/home/dataset/go/bin/api_server" >> $FILE
-echo "ExecReload=/bin/kill -HUP $MAINPID" >> $FILE
-echo "Restart=always" >> $FILE
-echo "RestartSec=5" >> $FILE
-echo "" >> $FILE
-echo "[Install]" >> $FILE
-echo "WantedBy=multi-user.target" >> $FILE
-echo "" >> $FILE
-echo "# sudo systemctl daemon-reload" >> $FILE
-echo "" >> $FILE
-echo "# sudo systemctl enable api_server" >> $FILE
-echo "# sudo systemctl start api_server" >> $FILE
-echo "# sudo systemctl stop api_server" >> $FILE
-echo "# sudo systemctl restart api_server" >> $FILE
-echo "# sudo systemctl reload api_server" >> $FILE
-echo "# sudo systemctl status api_server" >> $FILE
-echo "# sudo systemctl disable api_server" >> $FILE
-
-sudo cp $FILE /etc/systemd/system/fcbh_dataset_io.service
-
 # End of ec2-user commands
 EOF
 
@@ -273,16 +212,3 @@ chown -R ec2-user:ec2-user /home/ec2-user/.conda
 
 # Set proper permissions
 chmod -R u+rwX,go-w /opt/conda
-
-sudo -u ec2-user bash << 'EOF2'
-
-source /home/ec2-user/.bashrc
-cd $GOPROJ/dataset/
-go test ./tests -run TestTSBB
-
-go install dataset/controller/queue_server
-
-#/home/ec2-user/go/bin/queue_server &
-
-
-EOF2
