7b71f85ea02f61ed2bb58d0034c00c60ee814bd0
gary <gary@shortsands.com>
2025-02-27 17:09:09 -0500

    modified EC2_start_script.sh

--- EC2_start_script.sh
+++ EC2_start_script.sh
@@ -1,6 +1,11 @@
 #!/bin/bash -v
-cd /home/ec2-user/go/src/fcbh-dataset-io
+
+su - ec2-user << 'EOF'
+env
+cd ~/go/src/fcbh-dataset-io
 git pull
-/usr/local/go/bin/go install ./controller/queue_server
-cd /home/ec2-user
-nohup /home/ec2-user/go/bin/queue_server &
+go install ./controller/queue_server
+cd
+nohup ~go/bin/queue_server &
+EOF
+exit 0
