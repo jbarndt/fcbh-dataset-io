43fe318260c04d50f1f5e38692ca50fc6d9f1611
gary <gary@shortsands.com>
2025-02-27 23:39:36 -0500

    modified EC2_start_script.sh

--- input/utility.go
+++ input/utility.go
@@ -246,7 +246,8 @@ func ParseVOXAudioFilename(ctx context.Context, file *InputFile) *log.Status {
 	} else if parts[0][0] == 'O' {
 		file.Testament = `OT`
 	} else if parts[0][0] == 'P' {
-		file.Testament = `PT` // what should this really be
+		//file.Testament = `PT` // what should this really be
+		file.Testament = db.Testament(file.BookId)
 	} else {
 		return log.ErrorNoErr(ctx, 500, "Unknown media type", parts[0])
 	}
